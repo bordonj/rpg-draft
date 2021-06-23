dc1a6db49198988b146fb148f8884e7d7aca75db
bendunhampdx <bendunhampdx@gmail.com>
2021-06-23 14:14:00 -0700

    add CharacterClasses logic, and pass test to create this object
    Co-authored-by: Jenn B <jennifer.bordon@gmail.com>
    Co-authored-by: Erika D <erika.debelis@gmail.com>
    Co-authored-by: Kody G <gordon.km89@gmail.com>

--- __tests__/rpg.test.js
+++ __tests__/rpg.test.js
@@ -1,16 +1,23 @@
 // example commits for the following:
   // Good Fail: Add first test and Example Class.
   // Passing: Add constructor logic, pass first test.
-import {CharacterClass} from '../src/js/rpg.js'
+import {CharacterClasses} from '../src/js/rpg.js'
 
-describe('CharacterClass', () => {
+describe('CharacterClasses', () => {
 
   test('should correctly create a character class object with different stats properties', () => {
-    const character1 = new CharacterClass(100, 100, 10, 10, 10);
-    expect(character1.hp).toEqual(100);
-    expect(character1.mp).toEqual(100);
-    expect(character1.str).toEqual(10);
-    expect(character1.spd).toEqual(10);
-    expect(character1.def).toEqual(10);
+    const characterClasses = new CharacterClasses();
+    expect(characterClasses).toEqual({"assassin": {"def": 3, "hp": 70, "int": 10, "mp": 70, "res": 5, "spd": 8, "stl": 10, "str": 8}, "barbarian": {"def": 10, "hp": 125, "int": 0, "mp": 30, "res": 2, "spd": 4, "stl": 3, "str": 10}, "mage": {"def": 3, "hp": 50, "int": 10, "mp": 100, "res": 10, "spd": 4, "stl": 3, "str": 4}, "necromancer": {"def": 5, "hp": 50, "int": 5, "mp": 100, "res": 9, "spd": 5, "stl": 5, "str": 5}});
   });
+  // test('add respectice stats to the specific character class chosen', () => {
+  //   const character1 = new CharacterClass();
+  //   character1.addCharClass(mage);
+  //   expect(character1.hp).toEqual(50);
+  //   expect(character1.mp).toEqual(100);
+  //   expect(character1.str).toEqual(4);
+  //   expect(character1.spd).toEqual(3);
+  //   expect(character1.def).toEqual(3);
+  // })
 });
+
+
--- src/js/rpg.js
+++ src/js/rpg.js
@@ -1,10 +1,51 @@
-export class CharacterClass {
-  constructor(hp, mp, str, spd, def){
-    this.hp = hp;
-    this.mp = mp;
-    this.str = str;
-    this.spd = spd;
-    this.def = def;
+export class CharacterClasses {
+  constructor() {
+    this.mage = { //184
+      hp: 50,
+      mp: 100,
+      str: 4,
+      spd: 4,
+      def: 3,
+      int: 10,
+      stl: 3,
+      res: 10
+    };
+    this.barbarian = { //184
+      hp: 125,
+      mp: 30,
+      str: 10,
+      spd: 4,
+      def: 10,
+      int: 0,
+      stl: 3,
+      res: 2
+    };
+    this.assassin = { //184
+      hp: 70,
+      mp: 70,
+      str: 8,
+      spd: 8,
+      def: 3,
+      int: 10,
+      stl: 10,
+      res: 5
+    };
+    this.necromancer = { //184
+      hp: 50,
+      mp: 100,
+      str: 5,
+      spd: 5,
+      def: 5,
+      int: 5,
+      stl: 5,
+      res: 9
+    };
+    return 'CharacterClasses made';
   }
-
 }
+
+// export class  {
+//   constructor(wizard, warrior, etc) {
+//     player = new Player (Wizard, 100, 100, 10, 10, 10)
+//   }
+// }
